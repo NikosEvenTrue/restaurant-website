e6d43d445ebc052f4c71bb9de91dc7d34d832c92
Niko Seven <nikita.xakimov.77@gmail.com>
2025-03-27 11:23:15 +0300

    fixed viewport jumping on mobile browsers (vh, vmin)

--- app.js
+++ app.js
@@ -8,3 +8,49 @@ const mobile_nav_buttons = document.getElementsByClassName("mobile-nav-button");
         })
     })
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    let vh = window.innerHeight * 0.01
+    let vw = window.outerWidth * 0.01
+    let vmin = Math.min(vh, vw)
+
+    function setViewPort() {
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+        document.documentElement.style.setProperty('--vmin', `${vmin}px`)
+    }
+
+    window.addEventListener('orientationchange', () => {
+        [vh, vw] = [vw, vh]
+        setViewPort()
+    })
+
+    setViewPort()
+})
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     function setVH() {
+//         const vh = window.outerHeight * 0.01
+//         document.documentElement.style.setProperty('--vh', `${vh}px`)
+//     }
+
+//     function setVMin() {
+//         const vmin = Math.min(window.outerHeight * 0.01, window.outerWidth * 0.01)
+//         document.documentElement.style.setProperty('--vmin', `${vmin}px`)
+//     }
+
+//     function setViewPort() {
+//         setTimeout(() => {
+//             console.log(`${window.outerHeight} ${window.innerHeight}`)
+//             setVH()
+//             setVMin()
+//         }, 30)
+//     }
+
+//     window.addEventListener('orientationchange', () => {
+//         requestAnimationFrame(() => {
+//             setViewPort()
+//         })
+//     })
+
+//     setViewPort()
+// })
